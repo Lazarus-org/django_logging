075e805cf068857967987cdd7b9473e422a20dd0
MEHRSHAD MIRSHEKARY <mehrshad_mirshekary@email.com>
2024-08-20 14:46:37 +0330

    :sparkles::zap: feat(constants): add default_settings & type annotations file - Introduced default settings for logging configurations, such as LOG_DIR, LOG_FILE_LEVELS, and LOG_DATE_FORMAT. - Refactored the package to include config types using type annotations to improve code clarity and maintainability.
    This commit enhances the robustness and readability of the Django logging package, making it easier to maintain and extend.

---
+++ django_logging/constants/settings_types.py
@@ -0,0 +1,32 @@
+from typing import TypedDict, Union, List, Literal
+
+FormatOption = Union[int, str]
+
+
+class LogEmailNotifierType(TypedDict, total=False):
+    ENABLE: bool
+    NOTIFY_ERROR: bool
+    NOTIFY_CRITICAL: bool
+    LOG_FORMAT: FormatOption
+    USE_TEMPLATE: bool
+
+
+LogLevel = Literal["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"]
+
+
+class LogFileFormatsType(TypedDict, total=False):
+    DEBUG: FormatOption
+    INFO: FormatOption
+    WARNING: FormatOption
+    ERROR: FormatOption
+    CRITICAL: FormatOption
+
+
+# Type Aliases for other configurations
+LOG_DIR_TYPE = str
+LOG_FILE_LEVELS_TYPE = List[str]
+LOG_DATE_FORMAT_TYPE = str
+INITIALIZATION_MESSAGE_ENABLED_TYPE = bool
+LOG_CONSOLE_LEVEL_TYPE = LogLevel
+LOG_CONSOLE_FORMAT_TYPE = FormatOption
+LOG_CONSOLE_COLORIZE_TYPE = bool
