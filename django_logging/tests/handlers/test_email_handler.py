4c10232b7f50b24b429cd4315a2625d7e70f7e23
MEHRSHAD MIRSHEKARY <mehrshad_mirshekary@email.com>
2024-08-25 21:00:31 +0330

    :hammer::zap: refactor(tests): update handlers & middleware tests Added custom markers to simplify runnig test on each part Refactored tests and Implement class base tests on each module

--- django_logging/tests/handlers/test_email_handler.py
+++ django_logging/tests/handlers/test_email_handler.py
@@ -6,221 +6,194 @@ from django.conf import settings
 
 from django_logging.handlers.email_handler import EmailHandler
 
+pytestmark = [pytest.mark.handlers, pytest.mark.email_handler]
 
-@pytest.fixture
-def log_record() -> logging.LogRecord:
-    """
-    Fixture to create a dummy log record.
 
-    Returns:
-    -------
-    logging.LogRecord
-        A dummy log record with predefined attributes for testing.
-    """
-    return logging.LogRecord(
-        name="test",
-        level=logging.ERROR,
-        pathname=__file__,
-        lineno=10,
-        msg="Test message",
-        args=(),
-        exc_info=None,
+class TestEmailHandler:
+
+    @patch("django_logging.handlers.email_handler.send_email_async")
+    @patch("django_logging.handlers.email_handler.EmailHandler.render_template")
+    @patch(
+        "django_logging.handlers.email_handler.use_email_notifier_template",
+        return_value=True,
     )
+    def test_emit_with_html_template(
+        self,
+        mock_use_template: MagicMock,
+        mock_render_template: MagicMock,
+        mock_send_email: MagicMock,
+        email_handler: EmailHandler,
+        error_log_record: logging.LogRecord,
+    ) -> None:
+        """
+        Test the emit method when HTML templates are used.
 
+        This test verifies that the EmailHandler's `emit` method correctly renders an HTML
+        template and sends an email when `use_email_notifier_template` is enabled.
 
-@pytest.fixture
-def email_handler() -> EmailHandler:
-    """
-    Fixture to create an EmailHandler instance.
+        Args:
+        ----
+        mock_use_template : MagicMock
+            Mock for the `use_email_notifier_template` function.
+        mock_render_template : MagicMock
+            Mock for the `render_template` method.
+        mock_send_email : MagicMock
+            Mock for the `send_email_async` function.
+        email_handler : EmailHandler
+            The EmailHandler instance being tested.
+        error_log_record : logging.LogRecord
+            The log record fixture used for testing.
 
-    Returns:
-    -------
-    EmailHandler
-        An instance of the EmailHandler class.
-    """
-    return EmailHandler()
+        Asserts:
+        -------
+        - `render_template` is called once with the correct arguments.
+        - `send_email_async` is called once with the expected email subject, HTML content, and recipients.
+        """
+        mock_render_template.return_value = "<html>Formatted Log</html>"
 
+        email_handler.emit(error_log_record)
 
-@patch("django_logging.handlers.email_handler.send_email_async")
-@patch("django_logging.handlers.email_handler.EmailHandler.render_template")
-@patch(
-    "django_logging.handlers.email_handler.use_email_notifier_template",
-    return_value=True,
-)
-def test_emit_with_html_template(
-    mock_use_template: MagicMock,
-    mock_render_template: MagicMock,
-    mock_send_email: MagicMock,
-    email_handler: EmailHandler,
-    log_record: logging.LogRecord,
-) -> None:
-    """
-    Test the emit method when HTML templates are used.
+        mock_render_template.assert_called_once_with("Test message", None)
+        mock_send_email.assert_called_once_with(
+            "New Log Record: ERROR",
+            "<html>Formatted Log</html>",
+            [settings.ADMIN_EMAIL],
+        )
 
-    This test verifies that the EmailHandler's `emit` method correctly renders an HTML
-    template and sends an email when `use_email_notifier_template` is enabled.
-
-    Args:
-    ----
-    mock_use_template : MagicMock
-        Mock for the `use_email_notifier_template` function.
-    mock_render_template : MagicMock
-        Mock for the `render_template` method.
-    mock_send_email : MagicMock
-        Mock for the `send_email_async` function.
-    email_handler : EmailHandler
-        The EmailHandler instance being tested.
-    log_record : logging.LogRecord
-        The log record fixture used for testing.
-
-    Asserts:
-    -------
-    - `render_template` is called once with the correct arguments.
-    - `send_email_async` is called once with the expected email subject, HTML content, and recipients.
-    """
-    mock_render_template.return_value = "<html>Formatted Log</html>"
-
-    email_handler.emit(log_record)
-
-    mock_render_template.assert_called_once_with("Test message", None)
-    mock_send_email.assert_called_once_with(
-        "New Log Record: ERROR", "<html>Formatted Log</html>", [settings.ADMIN_EMAIL]
+    @patch("django_logging.handlers.email_handler.send_email_async")
+    @patch(
+        "django_logging.handlers.email_handler.use_email_notifier_template",
+        return_value=False,
     )
+    def test_emit_without_html_template(
+        self,
+        mock_use_template: MagicMock,
+        mock_send_email: MagicMock,
+        error_log_record: logging.LogRecord,
+    ) -> None:
+        """
+        Test the emit method when HTML templates are not used.
 
+        This test checks that the EmailHandler's `emit` method correctly sends a plain text
+        email when `use_email_notifier_template` is disabled.
 
-@patch("django_logging.handlers.email_handler.send_email_async")
-@patch(
-    "django_logging.handlers.email_handler.use_email_notifier_template",
-    return_value=False,
-)
-def test_emit_without_html_template(
-    mock_use_template: MagicMock,
-    mock_send_email: MagicMock,
-    log_record: logging.LogRecord,
-) -> None:
-    """
-    Test the emit method when HTML templates are not used.
+        Args:
+        ----
+        mock_use_template : MagicMock
+            Mock for the `use_email_notifier_template` function.
+        mock_send_email : MagicMock
+            Mock for the `send_email_async` function.
+        error_log_record : logging.LogRecord
+            The log record fixture used for testing.
 
-    This test checks that the EmailHandler's `emit` method correctly sends a plain text
-    email when `use_email_notifier_template` is disabled.
+        Asserts:
+        -------
+        - `send_email_async` is called once with the expected email subject, plain text content, and recipients.
+        """
+        email_handler = EmailHandler()
+        email_handler.emit(error_log_record)
 
-    Args:
-    ----
-    mock_use_template : MagicMock
-        Mock for the `use_email_notifier_template` function.
-    mock_send_email : MagicMock
-        Mock for the `send_email_async` function.
-    log_record : logging.LogRecord
-        The log record fixture used for testing.
+        mock_send_email.assert_called_once_with(
+            "New Log Record: ERROR", "Test message", [settings.ADMIN_EMAIL]
+        )
 
-    Asserts:
-    -------
-    - `send_email_async` is called once with the expected email subject, plain text content, and recipients.
-    """
-    email_handler = EmailHandler()
-    email_handler.emit(log_record)
-
-    mock_send_email.assert_called_once_with(
-        "New Log Record: ERROR", "Test message", [settings.ADMIN_EMAIL]
+    @patch("django_logging.handlers.email_handler.EmailHandler.handleError")
+    @patch(
+        "django_logging.handlers.email_handler.send_email_async",
+        side_effect=Exception("Email send failed"),
     )
+    def test_emit_handles_exception(
+        self,
+        mock_send_email: MagicMock,
+        mock_handle_error: MagicMock,
+        email_handler: EmailHandler,
+        error_log_record: logging.LogRecord,
+    ) -> None:
+        """
+        Test that the emit method handles exceptions during email sending.
 
+        This test ensures that when an exception occurs during the email sending process,
+        the `handleError` method is called to manage the error.
 
-@patch("django_logging.handlers.email_handler.EmailHandler.handleError")
-@patch(
-    "django_logging.handlers.email_handler.send_email_async",
-    side_effect=Exception("Email send failed"),
-)
-def test_emit_handles_exception(
-    mock_send_email: MagicMock,
-    mock_handle_error: MagicMock,
-    email_handler: EmailHandler,
-    log_record: logging.LogRecord,
-) -> None:
-    """
-    Test that the emit method handles exceptions during email sending.
+        Args:
+        ----
+        mock_send_email : MagicMock
+            Mock for the `send_email_async` function.
+        mock_handle_error : MagicMock
+            Mock for the `handleError` method.
+        email_handler : EmailHandler
+            The EmailHandler instance being tested.
+        error_log_record : logging.LogRecord
+            The log record fixture used for testing.
 
-    This test ensures that when an exception occurs during the email sending process,
-    the `handleError` method is called to manage the error.
+        Asserts:
+        -------
+        - `handleError` is called once with the log record when an exception occurs.
+        """
+        email_handler.emit(error_log_record)
 
-    Args:
-    ----
-    mock_send_email : MagicMock
-        Mock for the `send_email_async` function.
-    mock_handle_error : MagicMock
-        Mock for the `handleError` method.
-    email_handler : EmailHandler
-        The EmailHandler instance being tested.
-    log_record : logging.LogRecord
-        The log record fixture used for testing.
+        mock_handle_error.assert_called_once_with(error_log_record)
 
-    Asserts:
-    -------
-    - `handleError` is called once with the log record when an exception occurs.
-    """
-    email_handler.emit(log_record)
-
-    mock_handle_error.assert_called_once_with(log_record)
-
-
-@patch(
-    "django_logging.handlers.email_handler.RequestLogMiddleware.get_ip_address",
-    return_value="127.0.0.1",
-)
-@patch(
-    "django_logging.handlers.email_handler.RequestLogMiddleware.get_user_agent",
-    return_value="Mozilla/5.0",
-)
-@patch("django_logging.handlers.email_handler.engines")
-def test_render_template(
-    mock_engines: MagicMock,
-    mock_get_user_agent: MagicMock,
-    mock_get_ip_address: MagicMock,
-) -> None:
-    """
-    Test the render_template method of EmailHandler.
-
-    This test verifies that the `render_template` method correctly renders the HTML
-    template with the provided log message and request details.
-
-    Args:
-    ----
-    mock_engines : MagicMock
-        Mock for the Django template engines.
-    mock_get_user_agent : MagicMock
-        Mock for the `get_user_agent` method of the middleware.
-    mock_get_ip_address : MagicMock
-        Mock for the `get_ip_address` method of the middleware.
-
-    Asserts:
-    -------
-    - The correct template is retrieved and rendered with the expected context data.
-    - The rendered HTML output matches the expected formatted log.
-
-    Returns:
-    -------
-    str
-        The rendered HTML output for the log message.
-    """
-    mock_template = MagicMock()
-    mock_template.render.return_value = "<html>Formatted Log</html>"
-    mock_django_engine = MagicMock()
-    mock_django_engine.get_template.return_value = mock_template
-    mock_engines.__getitem__.return_value = mock_django_engine
-
-    email_handler = EmailHandler()
-
-    mock_request = MagicMock()
-    rendered_output = email_handler.render_template("Test message", mock_request)
-
-    mock_django_engine.get_template.assert_called_once_with(
-        "email_notifier_template.html"
+    @patch(
+        "django_logging.handlers.email_handler.RequestLogMiddleware.get_ip_address",
+        return_value="127.0.0.1",
     )
-    mock_template.render.assert_called_once_with(
-        {
-            "message": "Test message",
-            "time": ANY,  # The actual time is not critical to the test
-            "browser_type": "Mozilla/5.0",
-            "ip_address": "127.0.0.1",
-        }
+    @patch(
+        "django_logging.handlers.email_handler.RequestLogMiddleware.get_user_agent",
+        return_value="Mozilla/5.0",
     )
-    assert rendered_output == "<html>Formatted Log</html>"
+    @patch("django_logging.handlers.email_handler.engines")
+    def test_render_template(
+        self,
+        mock_engines: MagicMock,
+        mock_get_user_agent: MagicMock,
+        mock_get_ip_address: MagicMock,
+    ) -> None:
+        """
+        Test the render_template method of EmailHandler.
+
+        This test verifies that the `render_template` method correctly renders the HTML
+        template with the provided log message and request details.
+
+        Args:
+        ----
+        mock_engines : MagicMock
+            Mock for the Django template engines.
+        mock_get_user_agent : MagicMock
+            Mock for the `get_user_agent` method of the middleware.
+        mock_get_ip_address : MagicMock
+            Mock for the `get_ip_address` method of the middleware.
+
+        Asserts:
+        -------
+        - The correct template is retrieved and rendered with the expected context data.
+        - The rendered HTML output matches the expected formatted log.
+
+        Returns:
+        -------
+        str
+            The rendered HTML output for the log message.
+        """
+        mock_template = MagicMock()
+        mock_template.render.return_value = "<html>Formatted Log</html>"
+        mock_django_engine = MagicMock()
+        mock_django_engine.get_template.return_value = mock_template
+        mock_engines.__getitem__.return_value = mock_django_engine
+
+        email_handler = EmailHandler()
+
+        mock_request = MagicMock()
+        rendered_output = email_handler.render_template("Test message", mock_request)
+
+        mock_django_engine.get_template.assert_called_once_with(
+            "email_notifier_template.html"
+        )
+        mock_template.render.assert_called_once_with(
+            {
+                "message": "Test message",
+                "time": ANY,  # The actual time is not critical to the test
+                "browser_type": "Mozilla/5.0",
+                "ip_address": "127.0.0.1",
+            }
+        )
+        assert rendered_output == "<html>Formatted Log</html>"
--- django_logging/tests/middleware/test_request_middleware.py
+++ django_logging/tests/middleware/test_request_middleware.py
@@ -1,193 +1,147 @@
 import logging
-from typing import Callable
 from unittest.mock import Mock
 
 import pytest
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import AnonymousUser
-from django.http import HttpResponse
 from django.test import RequestFactory
 from django_logging.middleware import RequestLogMiddleware
 
 
-@pytest.fixture
-def request_factory() -> RequestFactory:
-    """
-    Fixture to create a RequestFactory instance for generating request objects.
-
-    Returns:
-    -------
-    RequestFactory
-        An instance of RequestFactory for creating mock requests.
-    """
-    return RequestFactory()
+pytestmark = [pytest.mark.middleware, pytest.mark.request_middleware]
 
 
-@pytest.fixture
-def get_response() -> Callable:
-    """
-    Fixture to create a mock get_response function.
+class TestRequestMiddleware:
 
-    Returns:
-    -------
-    function
-        A function that returns an HttpResponse with a dummy response.
-    """
+    def test_authenticated_user_logging(
+        self,
+        request_middleware: RequestLogMiddleware,
+        request_factory: RequestFactory,
+        caplog: pytest.LogCaptureFixture,
+    ) -> None:
+        """
+        Test logging of requests for authenticated users.
 
-    def _get_response(request: RequestFactory) -> HttpResponse:
-        return HttpResponse("Test Response")
+        This test verifies that when an authenticated user makes a request,
+        the relevant request information, including the username, is logged.
 
-    return _get_response
+        Args:
+        ----
+        request_middleware : RequestLogMiddleware
+            The middleware instance used to process the request.
+        request_factory : RequestFactory
+            A factory for creating mock HTTP requests.
+        caplog : pytest.LogCaptureFixture
+            A fixture for capturing log messages.
 
+        Asserts:
+        -------
+        - "Request Info" is present in the logs.
+        - The requested path is logged.
+        - The username of the authenticated user is logged.
+        - The request object has `ip_address` and `browser_type` attributes.
+        """
+        request = request_factory.get("/test-path")
 
-@pytest.fixture
-def middleware(get_response: Callable) -> RequestLogMiddleware:
-    """
-    Fixture to create an instance of RequestLogMiddleware.
+        UserModel = get_user_model()
+        username_field = UserModel.USERNAME_FIELD
 
-    Args:
-    ----
-    get_response : function
-        A function that returns an HttpResponse for a given request.
+        request.user = Mock()
+        request.user.is_authenticated = True
+        setattr(request.user, username_field, "test_user")
 
-    Returns:
-    -------
-    RequestLogMiddleware
-        An instance of RequestLogMiddleware with the provided get_response function.
-    """
-    return RequestLogMiddleware(get_response)
+        with caplog.at_level(logging.INFO):
+            request_middleware(request)
 
+        assert "Request Info" in caplog.text
+        assert "test-path" in caplog.text
+        assert "test_user" in caplog.text
+        assert request.ip_address
+        assert request.browser_type
 
-def test_authenticated_user_logging(
-    middleware: RequestLogMiddleware,
-    request_factory: RequestFactory,
-    caplog: pytest.LogCaptureFixture,
-) -> None:
-    """
-    Test logging of requests for authenticated users.
+    def test_anonymous_user_logging(
+        self,
+        request_middleware: RequestLogMiddleware,
+        request_factory: RequestFactory,
+        caplog: pytest.LogCaptureFixture,
+    ) -> None:
+        """
+        Test logging of requests for anonymous users.
 
-    This test verifies that when an authenticated user makes a request,
-    the relevant request information, including the username, is logged.
+        This test ensures that when an anonymous user makes a request,
+        the relevant request information, including the identification as "Anonymous", is logged.
 
-    Args:
-    ----
-    middleware : RequestLogMiddleware
-        The middleware instance used to process the request.
-    request_factory : RequestFactory
-        A factory for creating mock HTTP requests.
-    caplog : pytest.LogCaptureFixture
-        A fixture for capturing log messages.
+        Args:
+        ----
+        request_middleware : RequestLogMiddleware
+            The middleware instance used to process the request.
+        request_factory : RequestFactory
+            A factory for creating mock HTTP requests.
+        caplog : pytest.LogCaptureFixture
+            A fixture for capturing log messages.
 
-    Asserts:
-    -------
-    - "Request Info" is present in the logs.
-    - The requested path is logged.
-    - The username of the authenticated user is logged.
-    - The request object has `ip_address` and `browser_type` attributes.
-    """
-    request = request_factory.get("/test-path")
+        Asserts:
+        -------
+        - "Request Info" is present in the logs.
+        - The request is identified as coming from an "Anonymous" user.
+        """
+        request = request_factory.get("/test-path")
+        request.user = AnonymousUser()
 
-    UserModel = get_user_model()
-    username_field = UserModel.USERNAME_FIELD
+        with caplog.at_level(logging.INFO):
+            request_middleware(request)
 
-    request.user = Mock()
-    request.user.is_authenticated = True
-    setattr(request.user, username_field, "test_user")
+        assert "Request Info" in caplog.text
+        assert "Anonymous" in caplog.text
 
-    with caplog.at_level(logging.INFO):
-        middleware(request)
+    def test_ip_address_extraction(
+        self, request_middleware: RequestLogMiddleware, request_factory: RequestFactory
+    ) -> None:
+        """
+        Test extraction of the client's IP address from the request.
 
-    assert "Request Info" in caplog.text
-    assert "test-path" in caplog.text
-    assert "test_user" in caplog.text
-    assert request.ip_address
-    assert request.browser_type
+        This test verifies that the middleware correctly extracts the IP address
+        from the `HTTP_X_FORWARDED_FOR` header in the request.
 
+        Args:
+        ----
+        request_middleware : RequestLogMiddleware
+            The middleware instance used to process the request.
+        request_factory : RequestFactory
+            A factory for creating mock HTTP requests.
 
-def test_anonymous_user_logging(
-    middleware: RequestLogMiddleware,
-    request_factory: RequestFactory,
-    caplog: pytest.LogCaptureFixture,
-) -> None:
-    """
-    Test logging of requests for anonymous users.
+        Asserts:
+        -------
+        - The `ip_address` attribute of the request is correctly set to the value in the `HTTP_X_FORWARDED_FOR` header.
+        """
+        request = request_factory.get("/test-path", HTTP_X_FORWARDED_FOR="192.168.1.1")
 
-    This test ensures that when an anonymous user makes a request,
-    the relevant request information, including the identification as "Anonymous", is logged.
+        request_middleware(request)
 
-    Args:
-    ----
-    middleware : RequestLogMiddleware
-        The middleware instance used to process the request.
-    request_factory : RequestFactory
-        A factory for creating mock HTTP requests.
-    caplog : pytest.LogCaptureFixture
-        A fixture for capturing log messages.
+        assert request.ip_address == "192.168.1.1"
 
-    Asserts:
-    -------
-    - "Request Info" is present in the logs.
-    - The request is identified as coming from an "Anonymous" user.
-    """
-    request = request_factory.get("/test-path")
-    request.user = AnonymousUser()
+    def test_user_agent_extraction(
+        self, request_middleware: RequestLogMiddleware, request_factory: RequestFactory
+    ) -> None:
+        """
+        Test extraction of the client's user agent from the request.
 
-    with caplog.at_level(logging.INFO):
-        middleware(request)
+        This test verifies that the middleware correctly extracts the user agent
+        from the `HTTP_USER_AGENT` header in the request.
 
-    assert "Request Info" in caplog.text
-    assert "Anonymous" in caplog.text
+        Args:
+        ----
+        request_middleware : RequestLogMiddleware
+            The middleware instance used to process the request.
+        request_factory : RequestFactory
+            A factory for creating mock HTTP requests.
 
+        Asserts:
+        -------
+        - The `browser_type` attribute of the request is correctly set to the value in the `HTTP_USER_AGENT` header.
+        """
+        request = request_factory.get("/test-path", HTTP_USER_AGENT="Mozilla/5.0")
 
-def test_ip_address_extraction(
-    middleware: RequestLogMiddleware, request_factory: RequestFactory
-) -> None:
-    """
-    Test extraction of the client's IP address from the request.
+        request_middleware(request)
 
-    This test verifies that the middleware correctly extracts the IP address
-    from the `HTTP_X_FORWARDED_FOR` header in the request.
-
-    Args:
-    ----
-    middleware : RequestLogMiddleware
-        The middleware instance used to process the request.
-    request_factory : RequestFactory
-        A factory for creating mock HTTP requests.
-
-    Asserts:
-    -------
-    - The `ip_address` attribute of the request is correctly set to the value in the `HTTP_X_FORWARDED_FOR` header.
-    """
-    request = request_factory.get("/test-path", HTTP_X_FORWARDED_FOR="192.168.1.1")
-
-    middleware(request)
-
-    assert request.ip_address == "192.168.1.1"
-
-
-def test_user_agent_extraction(
-    middleware: RequestLogMiddleware, request_factory: RequestFactory
-) -> None:
-    """
-    Test extraction of the client's user agent from the request.
-
-    This test verifies that the middleware correctly extracts the user agent
-    from the `HTTP_USER_AGENT` header in the request.
-
-    Args:
-    ----
-    middleware : RequestLogMiddleware
-        The middleware instance used to process the request.
-    request_factory : RequestFactory
-        A factory for creating mock HTTP requests.
-
-    Asserts:
-    -------
-    - The `browser_type` attribute of the request is correctly set to the value in the `HTTP_USER_AGENT` header.
-    """
-    request = request_factory.get("/test-path", HTTP_USER_AGENT="Mozilla/5.0")
-
-    middleware(request)
-
-    assert request.browser_type == "Mozilla/5.0"
+        assert request.browser_type == "Mozilla/5.0"
