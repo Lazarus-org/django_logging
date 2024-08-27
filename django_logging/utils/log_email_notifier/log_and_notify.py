c481756e8ab197b5254e77b14bf6f14ff7056fc0
MEHRSHAD MIRSHEKARY <mehrshad_mirshekary@email.com>
2024-08-27 22:32:34 +0330

    :zap::hammer: refactor(utils): Update log_email_notifier modules Refactored log_and_notify exception to improve clearity of Exception that might raised Updated imports in notifier to sort them

--- django_logging/utils/log_email_notifier/log_and_notify.py
+++ django_logging/utils/log_email_notifier/log_and_notify.py
@@ -1,25 +1,25 @@
-import logging
 import inspect
-from typing import Optional, Dict, Any
+import logging
+from typing import Any, Dict, Optional
 
 from django.conf import settings
 
 from django_logging.constants.log_format_options import FORMAT_OPTIONS
-from django_logging.utils.log_email_notifier.notifier import send_email_async
-from django_logging.utils.get_conf import get_config
 from django_logging.handlers import EmailHandler
 from django_logging.settings.conf import LogConfig
+from django_logging.utils.get_conf import get_config
+from django_logging.utils.log_email_notifier.notifier import send_email_async
 
 
 def log_and_notify_admin(
-    logger: logging.Logger, level: int, message: str, extra: Optional[Dict[str, Any]] = None
+    logger: logging.Logger,
+    level: int,
+    message: str,
+    extra: Optional[Dict[str, Any]] = None,
 ) -> None:
     # Get the caller's frame to capture the correct module, file, and line number
-    frame = inspect.currentframe().f_back
-    logging_settings = get_config()
-    email_notifier_enable = getattr(
-        logging_settings, "log_email_notifier_enable", False
-    )
+    frame = inspect.currentframe().f_back  # type: ignore
+    email_notifier_enable = get_config().get("log_email_notifier_enable", False)
 
     if not email_notifier_enable:
         raise ValueError(
@@ -27,30 +27,28 @@ def log_and_notify_admin(
             " in DJANGO_LOGGING in your settings to activate email notifications."
         )
 
-    _format = getattr(
-        logging_settings, "log_email_notifier_log_format", FORMAT_OPTIONS[1]
-    )
+    _format = get_config().get("log_email_notifier_log_format", FORMAT_OPTIONS[1])
 
     try:
         # create a LogRecord
         log_record = logger.makeRecord(
             name=logger.name,
             level=level,
-            fn=frame.f_code.co_filename,
-            lno=frame.f_lineno,
+            fn=frame.f_code.co_filename,  # type: ignore
+            lno=frame.f_lineno,  # type: ignore
             msg=message,
             args=(),
             exc_info=None,
-            func=frame.f_code.co_name,
+            func=frame.f_code.co_name,  # type: ignore
             extra=extra,
         )
 
         # Pass the LogRecord to the logger's handlers
         logger.handle(log_record)
-    except TypeError as e:
+    except (TypeError, AttributeError) as e:
         raise ValueError(
             f"Failed to log message due to invalid param. Original error: {e}"
-        )
+        ) from e
     # Create a formatter instance and pass the email_notifier format
     formatter = logging.Formatter(LogConfig.resolve_format(_format))
 
