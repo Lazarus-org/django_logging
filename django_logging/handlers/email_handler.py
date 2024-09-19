4dc7db1db0f16b8e5dc50443d92c037583bc984f
MEHRSHAD MIRSHEKARY <mehrshad_mirshekary@email.com>
2024-09-19 13:47:41 +0330

    :books: docs(handlers): add detailed docstrings to EmailHandler class and methods
    Added comprehensive docstrings to the EmailHandler class and its methods
    (emit and render_template) to improve code documentation. These docstrings
    explain the functionality, arguments, and return values, enhancing code
    readability and maintainability.

--- django_logging/handlers/email_handler.py
+++ django_logging/handlers/email_handler.py
@@ -12,7 +12,40 @@ from django_logging.utils.log_email_notifier.notifier import send_email_async
 
 
 class EmailHandler(Handler):
+    """A custom logging handler that sends log records via email.
+
+    This handler formats log records, optionally renders them using an
+    email template, and sends the resulting email to the administrator's
+    email address defined in the Django settings.
+
+    Methods:
+    -------
+        emit(record: LogRecord) -> None:
+            Processes a log record and sends it via email to the administrator.
+
+        render_template(log_entry: str, request: Optional[HttpRequest] = None, template_path: str = "email_notifier_template.html") -> str:
+            Renders the email body using the provided log entry and optional request details.
+            The rendered email includes the log message, the current date and time,
+            the user's IP address, and browser information.
+
+    """
+
     def emit(self, record: LogRecord) -> None:
+        """Processes a log record and sends it via email.
+
+        This method retrieves the request from the log record (if available), formats
+        the log message, optionally renders the email body using a template, and sends
+        the email asynchronously to the administrator.
+
+        Args:
+        ----
+            record (LogRecord): The log record to be processed and sent via email.
+
+        Raises:
+        ------
+            Exception: If any error occurs while sending the email or formatting the log record.
+
+        """
         try:
             request = getattr(record, "request", None)
             log_entry = self.format(record)
@@ -34,6 +67,25 @@ class EmailHandler(Handler):
         request: Optional[HttpRequest] = None,
         template_path: str = "email_notifier_template.html",
     ) -> str:
+        """Renders the email body using a Django template.
+
+        This method uses the provided log entry and request (if available)
+        to generate an HTML email body. The email includes details such as the
+        log message, current date and time, the IP address, and browser type
+        of the user making the request.
+
+        Args:
+        ----
+            log_entry (str): The formatted log message to be included in the email.
+            request (Optional[HttpRequest]): The HTTP request associated with the log entry, if available.
+            template_path (str): The path to the Django template to be used for rendering the email.
+                                 Defaults to "email_notifier_template.html".
+
+        Returns:
+        -------
+            str: The rendered email body as a string.
+
+        """
         django_engine = engines["django"]
         template = django_engine.get_template(template_path)
 
